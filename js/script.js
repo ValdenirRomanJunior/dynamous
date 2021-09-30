8a53cf58f3adb0aeb0f114c3d65991b6b0f4fb35
Valdenir Roman Junior <vromanjunior@outlook.com>
2021-09-30 15:52:51 -0300

    site finalizado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
     document.querySelector(".hamburguer").addEventListener("click", ()=>
         document.querySelector(".container").classList.toggle("show-menu")
     );
+
+
+    document.querySelector(".menu").addEventListener("click", ()=>
+        document.querySelector(".container").classList.remove("show-menu"),
+        document.querySelector(".container").classList.toggle("close-menu")
+    );
